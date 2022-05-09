7962741c602ce80dc860abf138fcdb36a9d37ea9
GustavoAntonio12 <gustavo.antonio@sptech.school>
2022-05-09 18:19:41 -0300

    feat(Index): adicionando o componente

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import mapboxgl from '!mapbox-gl';
 
-function Map () {
+function Map() {
+    mapboxgl.accessToken = 'pkeyJ1IjoiaGliaXNjb2VudGVycHJpc2UiLCJhIjoiY2wyNTd1Nmd0MTVoODNjcDc5OXBmODYxMCJ9.PQo8LI7tQYbPRTdWbCJXSw'
+
     return (
         <>
-        
+        <div ref={mapContainer} className="map-container" />
         </>
     );
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import {MenuDonator} from "../components/Menu";
+import { MenuDonator } from "../components/Menu";
+import Map from "../components/Map";
 
 export default function Index() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
 
-    return(
-        <MenuDonator selected="home"/>
-    )
-    
+    return (
+        <>
+            <MenuDonator selected="home" />
+            <Map />
+        </>
+    );
+
 }
